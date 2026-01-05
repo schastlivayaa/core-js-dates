b84c745ebf5040e6b28d017855ff38f187e64e16
Zlata Schastlivaya <s4astlivaya.zlata11@yandex.ru>
2026-01-06 01:06:37 +0300

    feat: solve tasks isDateInPeriod, formatDate, getCountWeekendsInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -139,8 +139,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const d = new Date(date);
+  const start = new Date(period.start);
+  const end = new Date(period.end);
+
+  return start <= d && d <= end;
 }
 
 /**
@@ -154,8 +158,19 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const d = new Date(date);
+  const year = d.getUTCFullYear();
+  const month = d.getUTCMonth() + 1;
+  const day = d.getUTCDate();
+  let hh = d.getUTCHours();
+  const mm = String(d.getUTCMinutes()).padStart(2, '0');
+  const ss = String(d.getUTCSeconds()).padStart(2, '0');
+
+  const a = hh >= 12 ? 'PM' : 'AM';
+  hh = hh % 12 === 0 ? 12 : hh % 12;
+
+  return `${month}/${day}/${year}, ${hh}:${mm}:${ss} ${a}`;
 }
 
 /**
@@ -170,8 +185,16 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  let weekends = 0;
+  const lastDate = new Date(year, month, 0).getDate();
+
+  for (let date = 1; date <= lastDate; date += 1) {
+    const day = new Date(year, month - 1, date).getDay();
+    if (day === 0 || day === 6) weekends += 1;
+  }
+
+  return weekends;
 }
 
 /**
